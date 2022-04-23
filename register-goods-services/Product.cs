066bbe0f281f493c802574be1332a3e045de0a5a
Alex Lobov <penoplavaaan@gmail.com>
2022-04-23 17:25:08 +0500

    .

--- register-goods-services/Product.cs
+++ register-goods-services/Product.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.IO;
+using SQLite;
+
 namespace registergoodsservices
 {
-    public class OccupationModel
+    public class Product
     {
-        public OccupationModel()
-        {
-        }
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public float Price { get; set; }
+        public int Organisation_id { get; set; }
     }
 }
