dc3f4e11c3051e9ff46d8cc1d9fa1f2fa34e2126
Alex Lobov <penoplavaaan@gmail.com>
2022-05-07 14:45:09 +0500

    add auth, extend user

---
+++ register-goods-services/Controllers/AuthorisationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace registergoodsservices.Controllers
+{
+    public class AuthorisationController
+    {
+        public static string GetHash(HashAlgorithm hashAlgorithm, string input)
+        {
+
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            var sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        private static bool VerifyHash(HashAlgorithm hashAlgorithm, string input, string hash)
+        {
+            var hashOfInput = GetHash(hashAlgorithm, input);
+
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return comparer.Compare(hashOfInput, hash) == 0;
+        }
+    }
+}
