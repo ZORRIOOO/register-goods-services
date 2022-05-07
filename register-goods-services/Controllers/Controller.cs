58b81edc799d4e52302bf131d657c352dc52cbe0
Alex Lobov <penoplavaaan@gmail.com>
2022-05-07 13:27:55 +0500

    add base controller

---
+++ register-goods-services/Controllers/Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace registergoodsservices
+{
+    public abstract class Controller<T>
+            where T : Model, new()
+    {
+        protected readonly SQLiteAsyncConnection _database;
+
+        public Controller(string dbPath)
+        {
+            _database = (new Database<T>(dbPath)).database;
+        }
+
+        public Task<List<T>> GetAllRecordsAsync(int pageNumber, int pageSize = 10)
+        {
+            List<T> records = _database.Table<T>().ToListAsync().Result;
+
+            return (Task<List<T>>)GetPage(records, pageNumber, pageSize);
+        }
+
+        static IList<T> GetPage(IList<T> list, int pageNumber, int pageSize = 10)
+        {
+            return list.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+        }
+
+        public Task<T> GetRecordAsync(T record)
+        {
+            return _database.Table<T>().ElementAtAsync(record.ID);
+        }
+
+        public Task<int> SaveRecordAsync(T record)
+        {
+            return _database.InsertAsync(record);
+        }
+
+        public void DeleteRecordasync(T record)
+        {
+            _database.DeleteAsync(record);
+        }
+    }
+}
